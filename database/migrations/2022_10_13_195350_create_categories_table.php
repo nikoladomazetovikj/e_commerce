3e84a46bf7a031d7a5488f96f06d0a6b4f2ee1e9
Nikola-mkd <nikolad150@gmail.com>
2022-10-13 22:28:05 +0200

    #3 db migrations created

--- database/migrations/2022_10_13_195350_create_categories_table.php
+++ database/migrations/2022_10_13_195350_create_categories_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
---
+++ database/migrations/2022_10_13_195619_create_online_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('online_payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable()->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->bigInteger('seed_id')->nullable()->unsigned();
+            $table->foreign('seed_id')->references('id')->on('seeds')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('quantity');
+            $table->decimal('total_price');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('online_payments');
+    }
+};
