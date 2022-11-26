5d09bbed36679501f24410be5ed92c99c0090a66
nikolla.d <nikolad150@gmail.com>
2022-11-26 19:03:18 +0100

    feat: created enums for role and category

---
+++ app/Enums/Category.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum Category : int
+{
+    // TODO: create categories for seeds
+
+}
---
+++ app/Enums/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum Role : int {
+
+    case ADMIN = 1;
+    case CUSTOMER = 2;
+    case CONTENT_WRITER = 3;
+    case MANAGER = 4;
+}
