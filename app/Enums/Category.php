af7a355af704bd53eccfbcba483ff3bca209aa9d
nikolla.d <nikolad150@gmail.com>
2022-12-03 18:55:01 +0100

    category seeder

--- app/Enums/Category.php
+++ app/Enums/Category.php
@@ -4,6 +4,15 @@ namespace App\Enums;
 
 enum Category : int
 {
-    // TODO: create categories for seeds
-
+    // this will be default categories  for the seeds
+    case FLAX_SEED = 1;
+    case SESAME_SEED = 2;
+    case SUNFLOWER_SEED = 3;
+    case CHIA_SEED = 4;
+    case HEMP_SEED = 5;
+    case PUMPKIN_SEED = 6;
+    case POMEGRANATE_SEED = 7;
+    case QUINOA = 8;
+    case PINE_NUTS = 9;
+    case CARAWAY_SEED = 10;
 }
