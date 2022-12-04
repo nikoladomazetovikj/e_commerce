876ee8b8f4ee2e10ecebe0b4486cb798135cf2c1
nikolla.d <nikolad150@gmail.com>
2022-12-04 03:34:52 +0100

    site details controllers routes and request

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('content.writer')->group(function () {
+    Route::get('/site', [\App\Http\Controllers\SiteDetailsController::class, 'index']);
+});
+
 require __DIR__.'/auth.php';
