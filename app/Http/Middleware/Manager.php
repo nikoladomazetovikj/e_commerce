23a3129a62ed78e51baedc318964e1d93c0e463c
nikolla.d <nikolad150@gmail.com>
2022-12-04 03:12:32 +0100

    created middlewares

---
+++ app/Http/Middleware/Manager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Manager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::user()->role_id == Role::MANAGER->value) {
+            return abort(404);
+        }
+    }
+}
