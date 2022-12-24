af9df80da839834f8415b093c59deb8549fbd998
nikolla.d <nikolad150@gmail.com>
2022-12-24 21:10:54 +0100

    fix: seed controller first crud operations

--- app/Http/Controllers/SeedController.php
+++ app/Http/Controllers/SeedController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeedRequest;
+use App\Models\Category;
+use App\Models\Seed;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class SeedController extends Controller
 {
@@ -13,7 +17,9 @@ class SeedController extends Controller
      */
     public function index()
     {
-        //
+        $allSeeds = Seed::with('category')->get();
+
+        return view('seeds.index',compact('allSeeds'));
     }
 
     /**
@@ -23,7 +29,9 @@ class SeedController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+
+        return view('seeds.create', compact('categories'));
     }
 
     /**
@@ -32,9 +40,23 @@ class SeedController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SeedRequest $request)
     {
-        //
+        // get image random name
+        $rand = Str::random(100);
+        $img = $rand . '.' . $request->image->extension();
+        $request->image->move(public_path('images'), $img);
+
+        Seed::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $img,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'category_id' => $request->category_id
+        ]);
+
+        return redirect()->route('seeds.index')->with(['status' => 'Seed inserted']);
     }
 
     /**
@@ -45,7 +67,9 @@ class SeedController extends Controller
      */
     public function show($id)
     {
-        //
+        $seed = Seed::with('category')->where('id', $id)->get();
+
+        return view('seeds.show', compact('seed'));
     }
 
     /**
@@ -80,5 +104,10 @@ class SeedController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function archived()
+    {
+       //TODO:
     }
 }
