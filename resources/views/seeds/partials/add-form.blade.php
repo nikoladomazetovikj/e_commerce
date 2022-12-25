c9a00507d171a46bc458fa745fc3efe73b0f2d87
nikolla.d <nikolad150@gmail.com>
2022-12-25 13:01:00 +0100

    fix: list friendly name ion add category form

--- resources/views/seeds/partials/add-form.blade.php
+++ resources/views/seeds/partials/add-form.blade.php
@@ -58,10 +58,10 @@
        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
            <option selected>Choose a category</option>
            @foreach($categories as $category)
-               <option value="{{$category->id}}" >{{$category->name}}</option>
+               <option value="{{$category->id}}" >{{$category->friendly_name}}</option>
            @endforeach
-           <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
        </select>
+           <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
        </div>
 
 
