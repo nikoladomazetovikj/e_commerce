2ddc479c4840808bcb2fb91894606474877b15e8
nikolla.d <nikolad150@gmail.com>
2023-01-05 22:18:36 +0100

    fix: remove select seed option

--- resources/views/seeds/partials/add-form.blade.php
+++ resources/views/seeds/partials/add-form.blade.php
@@ -56,12 +56,11 @@
        <select name="category_id" id="categories" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
        focus:ring-blue-500
        focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-           <option selected>Choose a category</option>
            @foreach($categories as $category)
                <option value="{{$category->id}}" >{{$category->friendly_name}}</option>
            @endforeach
-       </select>
            <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
+       </select>
        </div>
 
 
