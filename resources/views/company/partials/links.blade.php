7ef608b0a4aa690d13bc867525df63143ed11fb5
nikolla.d <nikolad150@gmail.com>
2023-01-07 18:26:42 +0100

    fix: added breadcrumb  links in partials links blade

--- resources/views/company/partials/links.blade.php
+++ resources/views/company/partials/links.blade.php
@@ -1,11 +1,24 @@
 <hr>
 <br>
-<diV class="mt-5">
-    <x-nav-link :href="route('company.create')" :active="request()->routeIs('company.create')">
-        {{ __('Create') }}
-    </x-nav-link>
-    <span class="dark:text-indigo-300">/</span>
-    <x-nav-link :href="route('company.trashed')" :active="request()->routeIs('company.trashed')">
-        {{ __('Archived Companies') }}
-    </x-nav-link>
+<diV class="mt-2">
+    <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li>
+                <div class="flex items-center">
+                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                    <x-nav-link :href="route('company.create')" :active="request()->routeIs('company.create')">
+                        {{ __('Create') }}
+                    </x-nav-link>
+                </div>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                    <x-nav-link :href="route('company.trashed')" :active="request()->routeIs('company.trashed')">
+                        {{ __('Archived Companies') }}
+                    </x-nav-link>
+                </div>
+            </li>
+        </ol>
+    </nav>
 </diV>
