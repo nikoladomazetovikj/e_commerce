b101bb6d5a2e8d38b6cfd247e75161c688f172e6
nikolla.d <nikolad150@gmail.com>
2023-01-07 21:10:23 +0100

    fix: agreements routes only for managers

--- resources/views/company/partials/links.blade.php
+++ resources/views/company/partials/links.blade.php
@@ -19,22 +19,24 @@
                     </x-nav-link>
                 </div>
             </li>
-            <li aria-current="page">
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <x-nav-link :href="route('companyPayment.index')" :active="request()->routeIs('companyPayment.index')">
-                        {{ __('Agreements') }}
-                    </x-nav-link>
-                </div>
-            </li>
-            <li aria-current="page">
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <x-nav-link :href="route('companyPayment.create')" :active="request()->routeIs('companyPayment.create')">
-                        {{ __('Add new agreement') }}
-                    </x-nav-link>
-                </div>
-            </li>
+          @if(\Illuminate\Support\Facades\Auth::user()->role_id == \App\Enums\Role::MANAGER->value)
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <x-nav-link :href="route('companyPayment.index')" :active="request()->routeIs('companyPayment.index')">
+                            {{ __('Agreements') }}
+                        </x-nav-link>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <x-nav-link :href="route('companyPayment.create')" :active="request()->routeIs('companyPayment.create')">
+                            {{ __('Add new agreement') }}
+                        </x-nav-link>
+                    </div>
+                </li>
+            @endif
         </ol>
     </nav>
 </diV>
