0755fb8e2afb52431d7e8e38cde132f45244bef2
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-19 19:35:29 +0100

    feat(email): queueable emails

---
+++ app/Jobs/SendSaleEmails.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\Role;
+use App\Mail\SalesMail;
+use App\Models\Sale;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendSaleEmails implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $sales = Sale::with('seed.category')->whereDate('start', '<=', Carbon::now()->toDateString())->get()->toArray();
+
+        $data = [];
+
+        foreach ($sales as $sale) {
+            $data = [
+                'start' => $sale['start'],
+                'end' => $sale['end'],
+                'sale' => $sale['sale'],
+                'seed' => $sale['seed']['name'],
+                'old_price' => (int)$sale['seed']['price'],
+                'image' => $sale['seed']['image'],
+                'category' => $sale['seed']['category']['friendly_name'],
+                'total_price' => $sale['seed']['price'] - ($sale['seed']['price'] * ($sale['sale'] / 100))
+            ];
+        }
+
+        $users = User::where('role_id', Role::CUSTOMER->value)->first();
+
+        //dd($users->email);
+
+        //foreach ($users as $user) {
+            Mail::to($users->email)->send(new SalesMail($data));
+        //}
+
+    }
+}
