7b80919d3c2187657e893179b223bb7f899fbc51
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-24 00:27:46 +0100

    fix: included necessary files in master template

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -12,11 +12,11 @@
     @yield('meta')
     <!-- Latest compiled and minified Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    @yield('css')
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 
 <body>
-
+@include('frontend.layouts.navbar')
 
 @yield('content')
 
