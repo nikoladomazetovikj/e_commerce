02b6bf18b37cff97cb4c40e5f3837afdf1eab1c6
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-23 22:57:32 +0100

    feat(home): frontend home page as initial

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.seeds.index');
 })->name('home');
 
 Route::get('/dashboard', [HomeController::class, 'index'])->middleware(['auth'])->name('dashboard');
