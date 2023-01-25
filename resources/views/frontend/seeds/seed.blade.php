e6e17fecd8ed1b6bc209afa706b48a9872ded534
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-26 00:23:33 +0100

    feat(product): add price

--- resources/views/frontend/seeds/seed.blade.php
+++ resources/views/frontend/seeds/seed.blade.php
@@ -5,12 +5,30 @@
     <div class="container">
         <div class="row">
             @foreach($seed as $s)
-            <div class="col-md-6">
-                <img src="{{asset('images/' . $s->image)}}">
+            <div class="col-md-3">
+                <img src="{{asset('images/' . $s->image)}}" class="img-fluid">
+               <div class="row">
+                   <div class="col-6">
+                       <a href="#">
+                           <button type="button" class="btn btn-success mt-3">Add to Cart</button>
+                       </a>
+                   </div>
+                   <div class="col-6 mt-3 border border-top-0 border-start-0 border-bottom-0">
+                      @if($s->sale != null)
+                           <p class="text-success text-end"> {{ $s->price - ($s->price *
+                            ($s->sale->sale / 100)
+                            )}} $</p>
+                           <p class="text-end text-danger text-decoration-line-through">{{$s->price}} $</p>
+                       @else
+                           <p class="text-end">{{$s->price}} $</p>
+                       @endif
+                   </div>
+               </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-9">
                 <h3>{{$s->name}}</h3>
                 <p>{{$s->category->friendly_name}}</p>
+                <hr>
                 {!! $s->description !!}
             </div>
             @endforeach
