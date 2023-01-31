d3cee19a9185b2485ef13edb3e8a49e91e420bab
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-31 22:17:08 +0100

    feat(products): render data from api

--- resources/views/frontend/seeds/seeds.blade.php
+++ resources/views/frontend/seeds/seeds.blade.php
@@ -10,7 +10,7 @@
                              <h5 class="">Categories</h5>
                              <form action="" id="category">
                                  <div class="col-lg-4" id="menu">
-                                     <input type="radio" class="btn-check" id="all" autocomplete="off" name="academies" value="all">
+                                     <input type="radio" class="btn-check" id="all" autocomplete="off" name="category" value="all">
                                      <label class="font-size-skills btn-block m-1 btn btn-outline-success rounded text-wrap filter-width p-1" for="all">All</label>
                                      @foreach($categories as $category)
                                          <input type="radio" class="btn-check category" id="#{{$category->id}}"
@@ -25,28 +25,30 @@
                  </div>
              </div>
              <div class="col-9 border border-top-0 border-end-0 border-bottom-0">
-                <div class="row">
-                    <div class="col-3">
-                        <div class="card">
-                            <img id="best-sales-img" src="" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title"></h5>
-                                        <p class="card-text"> </p>
-                                        <a href="" class="btn btn-success">Buy
-                                            Now</a>
-                                    </div>
-                                    <div class="col">
-                                        <h4 class="text-end text-success"> $</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="row" id="products">
+
                 </div>
+                 <div class="row">
+                     <div class="container-fluid">
+                         <div class="container mt-5">
+                             <div class="row">
+                                 <div class="col-lg-6 offset-lg-6 col-md-8 offset-md-3 col-12">
+                                     <div class="col-12 " id="pagination">
+
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
              </div>
          </div>
      </div>
  </div>
+@endsection
+
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.4.1/jquery.twbsPagination.min.js"> </script>
+    <script src="{{asset('js/filters.js')}}"></script>
 @endsection
