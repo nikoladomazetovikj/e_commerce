7705c2338d235285affabe3065b332b265e7deb2
nikoladomazetovikj <nikolad150@gmail.com>
2023-02-05 03:44:58 +0100

    fix(mail): queueable mails

--- app/Mail/SalesMail.php
+++ app/Mail/SalesMail.php
@@ -3,12 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SalesMail extends Mailable
+class SalesMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
