6a0d3f41622a8ec1b3ba0af5abdf686c0b3c2ff3
nikoladomazetovikj <nikolad150@gmail.com>
2023-05-27 00:47:10 +0200

    feat: seed policy

---
+++ app/Policies/SeedPolicy.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\Role;
+use App\Models\Seed;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class SeedPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Seed  $seed
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Seed $seed)
+    {
+
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        if ($user->role_id == Role::ADMIN->value || $user->role_id == Role::MANAGER->value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Seed  $seed
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Seed $seed)
+    {
+        if ($user->role_id == Role::ADMIN->value || $user->role_id == Role::MANAGER->value || $user->role_id ==
+            Role::CONTENT_WRITER->value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Seed  $seed
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Seed $seed)
+    {
+        if ($user->role_id == Role::ADMIN->value || $user->role_id == Role::MANAGER->value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Seed  $seed
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Seed $seed)
+    {
+        if ($user->role_id == Role::ADMIN->value || $user->role_id == Role::MANAGER->value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Seed  $seed
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Seed $seed)
+    {
+        if ($user->role_id == Role::ADMIN->value || $user->role_id == Role::MANAGER->value) {
+            return true;
+        }
+
+        return false;
+    }
+}
